911661da2091757250daa7302539b5fccb3f730b
laflaMme <maks.ponomarenko.2021@mail.ru>
2023-10-19 13:26:37 +0300

    /

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const TelegramApi = require('node-telegram-bot-api')
 const {gameOptions, againOptions} = require('./options')
-const sequelize = require('./db');
-const UserModel = require('./models');
-
-const token = '1702630643:AAHXiY0MRDKeF1XcuGTxWmpgdcCdAgzt4gE'
+const token = '6481247976:AAFUM-PNR5c-HRYlT4zmnSvWRjEweTyI7UA'
 
 const bot = new TelegramApi(token, {polling: true})
 
@@ -11,21 +8,13 @@ const chats = {}
 
 
 const startGame = async (chatId) => {
-    await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен ее угадать!`);
+    await bot.sendMessage(chatId,   `Сейчас я загадаю цифру с 0 до 9, а ты должен ее угадать !`)
     const randomNumber = Math.floor(Math.random() * 10)
     chats[chatId] = randomNumber;
     await bot.sendMessage(chatId, 'Отгадывай', gameOptions);
 }
 
-const start = async () => {
-
-    try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-    } catch (e) {
-        console.log('Подключение к бд сломалось', e)
-    }
-
+const start = () => {
     bot.setMyCommands([
         {command: '/start', description: 'Начальное приветствие'},
         {command: '/info', description: 'Получить информацию о пользователе'},
@@ -34,43 +23,33 @@ const start = async () => {
 
     bot.on('message', async msg => {
         const text = msg.text;
-        const chatId = msg.chat.id;
+        const chatId = msg.chat.id
 
-        try {
-            if (text === '/start') {
-                await UserModel.create({chatId})
-                await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp')
-                return bot.sendMessage(chatId, `Добро пожаловать в телеграм бот автора ютуб канала ULBI TV`);
-            }
-            if (text === '/info') {
-                const user = await UserModel.findOne({chatId})
-                return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}, в игре у тебя правильных ответов ${user.right}, неправильных ${user.wrong}`);
-            }
-            if (text === '/game') {
-                return startGame(chatId);
-            }
-            return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!)');
-        } catch (e) {
-            return bot.sendMessage(chatId, 'Произошла какая то ошибочка!)');
+        if(text === '/start') {
+            await bot.sendSticker(chatId, 'https://chpic.su/_data/stickers/v/VProgramm/VProgramm_001.webp')
+            return bot.sendMessage(chatId, `Добро пожаловать!`)
         }
+        if (text === '/info') {
+            return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`)
+        }
+        if(text === '/game') {
+            return startGame(chatId)
+        }
+        return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!)')
 
     })
 
-    bot.on('callback_query', async msg => {
+    bot.on('callback_query',async msg => {
         const data = msg.data;
         const chatId = msg.message.chat.id;
-        if (data === '/again') {
+        if(data === '/again') {
             return startGame(chatId)
         }
-        const user = await UserModel.findOne({chatId})
-        if (data == chats[chatId]) {
-            user.right += 1;
-            await bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`, againOptions);
+        if (data === chats[chatId]) {
+            return bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`, againOptions)
         } else {
-            user.wrong += 1;
-            await bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`, againOptions);
+            return bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`, againOptions)
         }
-        await user.save();
     })
 }
 
